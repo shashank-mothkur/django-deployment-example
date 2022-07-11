0af31e8ccd19a6952cfff460c02b4f2d617e2cec
Shashank Mothkuru <mshashank2403@gmail.com>
2022-07-11 11:21:02 +0530

    'first'

---
+++ django_level_5/passwords/crack/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from crack import views
+
+app_name = 'basic_app'
+urlpatterns=[
+     path('register',views.register,name="register"),
+     path('user_login',views.user_login,name='user_login')
+]
